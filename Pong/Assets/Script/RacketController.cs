6695e21d6d56f01453ca15c8aaa16fbc117cfc76
Cem Güvenbaş <cemguvenbass@gmail.com>
2023-03-16 20:37:55 +0300

    First Commit

---
+++ Pong/Assets/Script/RacketController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float limitHorizontal = 2.2f;
+    private void Update()
+    {
+        var input = Input.GetAxis("Horizontal");
+        var newPosition = transform.position + (Vector3.right * (input * speed * Time.deltaTime));
+        newPosition.x = Mathf.Clamp(newPosition.x, -limitHorizontal, limitHorizontal);
+
+        transform.position = newPosition;
+    }
+}
